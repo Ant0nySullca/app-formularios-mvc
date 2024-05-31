c6a0b692d92dbc7563de2a26926e61bb5412a1c5
llBlackShadowll <jvelitquintana2@gmail.com>
2024-05-30 21:34:56 -0500

    Nuevo Proyecto

---
+++ src/main/java/pe/edu/idat/app_fomularios_mvc/AppFomulariosMvcApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fomularios_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFomulariosMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFomulariosMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_fomularios_mvc/AppFomulariosMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_fomularios_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFomulariosMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
